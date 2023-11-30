be7db64de1a3d554f719ff816106de45cf3ae6a6
amarcheluk <amarnathcs1995@gmail.com>
2023-11-30 16:24:18 +0530

    hh

--- src/test/java/gitTest.java
+++ src/test/java/gitTest.java
@@ -1,9 +1,20 @@
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class gitTest {
   @Test
   public void f() {
-	  
+//	  String  url= System.getProperty("url");
+//	  String browser= System.getProperty("browser");
+//	  
+//	gitbaseClassTest p=new gitbaseClassTest();
+//	  System.out.println();
 	  System.out.println("amargittest");
+	  
+//	System.out.println(browser+url);
+	  
+
+	  
+	 
   }
 }
